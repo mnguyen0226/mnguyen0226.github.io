1e6d3ed95bf54e65964af36d6392cd43395eb3f9
mnguyen0226 <mnguyen0226@vt.edu>
2021-12-03 02:21:05 -0500

    add new projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,27 +2,57 @@
 
 const data = [
   {
-    title: 'Project (On-going): Haptic Force and Position Feedback Robotic Arm in Virtual Reality',
+    title: 'Project (On-going): Haptic Force and Position Feedback Robotic Arm in Virtual Reality (Software Development & Robotics)',
     subtitle: 'Senior Design Project',
     link: 'https://drive.google.com/drive/folders/1_4Eo6_wKtha8TBZqV3oUdaru9eJ1Sfay?usp=sharing',
-    image: 'images/projects/force_bot_project.png',
-    date: 'October 2021',
+    image: 'images/projects/force_bot_project.jpg',
+    date: 'August 2021',
     desc:
-      'Build Robotic Arm which is compatible with Vive VR Headset and HaptX Gloves for allowing Haptic Force and Position Feedback. '
-      + 'Tools: Unity, C#, Robotics.',
+      'Build robotic arm which is compatible with Vive VR Headset and HaptX Gloves for allowing Haptic Force and Position Feedback. '
+      + 'Tools: Unity, C#, ROS Bridge, Robotics.',
   },
   {
-    title: 'Project (On-going): AI Flappy Hokie',
+    title: 'Project (On-going): Cyber-Physical Attack Detection, Localization, & Attribution Evaluation with Temporal Graph Neural Networks for Smart Farm Water Distribution Networks (Machine Learning)',
+    subtitle: 'AIA Lab Project',
+    link: '',
+    image: 'images/projects/empty.png',
+    date: 'August 2021',
+    desc:
+      'Anomaly Detection with Temporal Graph Convolutional Network for time-series data... '
+      + 'Tools: Graph Neural Network, Python, Pytorch, GPU/CUDA.',
+  },
+  {
+    title: 'Project (On-going): Performance Comparison & Analysis Between Q-Learning, A2C, and PPO in Bipedal Walker - ECE 5984 (Reinforcement Learning)',
+    subtitle: 'ECE 5984 Class Project',
+    link: '',
+    image: 'images/projects/empty.png',
+    date: 'October 2021',
+    desc:
+      'Implementation from scratch of Q-Learning, A2C, and PPO on Bipedal Walker environment... '
+      + 'Tools: Reinforcement Learning, Python, Pytorch, GPU/CUDA.',
+  },
+  {
+    title: 'Project (On-going): Effectiveness of Second-Order Optimization for Non-convex Machine Learning - ECE 5554 (Machine Learning)',
+    subtitle: 'ECE 5554 Class Project',
+    link: '',
+    image: 'images/projects/empty.png',
+    date: 'October 2021',
+    desc:
+      '...'
+      + 'Tools: First & Second order optimization algorithms, Python, Pytorch, GPU/CUDA.',
+  },
+  {
+    title: 'Project (On-going): AI Flappy Hokie - Software Development & Machine Learning',
     subtitle: 'Personal Project',
     link: 'https://github.com/mnguyen0226/ai_flappy_bird',
     image: 'images/projects/ai_hokie.jpg',
     date: 'September 2021',
     desc:
       'Build Flappy Bird Environment and implement NEAT/RL algorithms. '
-      + 'Tools: PyGame, Django, Python.',
+      + 'Tools: PyGame, Django, Python, GPU/CUDA.',
   },
   {
-    title: 'Mini Project: Essential Data Structures in Linux Kernel Programming - ECE 4414',
+    title: 'Mini Project: Essential Data Structures in Linux Kernel Programming - ECE 4414 (Software Development)',
     subtitle: 'Personal Project',
     link: 'https://github.com/mnguyen0226/essential_data_structures_for_linux_kernel',
     image: 'images/projects/LKP.jpg',
@@ -32,37 +62,37 @@ const data = [
       + 'Tools: C, Linux API.',
   },
   {
-    title: 'Mini Project: Gated Transformers Implementation in PyTorch',
+    title: 'Mini Project: Gated Transformers Implementation in PyTorch (Machine Learning)',
     subtitle: 'Personal Project',
     link: 'https://github.com/mnguyen0226/gated_transformers_nlp',
     image: 'images/projects/gated_transformers.jpg',
     date: 'June 2021',
     desc:
       'Implementation of "Stabilizing Transformers for Reinforcement Learning" paper. '
-      + 'Tools: PyTorch, NLP Torchtext dataset.',
+      + 'Tools: PyTorch, NLP Torchtext dataset, GPU/CUDA.',
   },
   {
-    title: 'Image Augmentations Comparisons for Deep Learning Performance - ECE 4580',
+    title: 'Image Augmentations Comparisons for Deep Learning Performance - ECE 4580 (Machine Learning)',
     subtitle: 'ECE 4580 Class Project',
     link: 'https://github.com/mnguyen0226/image-augmentation-dnn-performance',
     image: 'images/projects/image_augmentation_project.jpg',
     date: 'April 2021',
     desc:
       'Surveyed performances of several digital image preprocessing techniques for deep learning. '
-      + 'Tools: Pytorch, Python, Numpy.',
+      + 'Tools: Pytorch, Python, Numpy, GPU/CUDA.',
   },
   {
-    title: 'Neural Networks from Scratch - ECE 4424',
+    title: 'Neural Networks from Scratch - ECE 4424 (Machine Learning)',
     subtitle: 'ECE 4424 Class Project',
     link: 'https://github.com/mnguyen0226/Neural-Network-from-Scratch-MNIST-Classification',
     image: '/images/projects/mnist.jpg',
     date: 'November 2020',
     desc:
       'Built a 3-layer neural network for the image classification task. '
-      + 'Tools: Python, Numpy.',
+      + 'Tools: Python, Numpy, GPU/CUDA.',
   },
   {
-    title: 'GIS Virginia Database Parser - CS 3114 ',
+    title: 'GIS Virginia Database Parser - CS 3114 (Software Development)',
     subtitle: 'CS 3114 Class Project',
     link: 'https://github.com/mnguyen0226/gis-virginia-parser',
     image: '/images/projects/gis_project.jpg',
@@ -72,7 +102,7 @@ const data = [
       + 'Tools: Java, Hashtable, PR Quadtree, Bufferpool. Note: this project is locked due to Honor Code, please contact me if you want a copy.',
   },
   {
-    title: 'Smart Home Project - ECE 2804 ',
+    title: 'Smart Home Project - ECE 2804 (Software Development & Robotics)',
     subtitle: 'ECE 2804 Class Project',
     link: 'https://github.com/mnguyen0226/smart-home-project-ECE-2804',
     image: '/images/projects/smart_home_project.jpg',
