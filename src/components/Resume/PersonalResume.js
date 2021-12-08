1a2395df7df40976a2ed27bc5b9eec07d10704fd
mnguyen0226 <mnguyen0226@vt.edu>
2021-12-08 00:03:56 -0500

    minor change

--- src/components/Resume/PersonalResume.js
+++ src/components/Resume/PersonalResume.js
@@ -5,7 +5,8 @@ const Presume = () => (
   <div className="references">
     <div className="link-to" id="references" />
     <div className="title">
-      <a href="https://github.com/mnguyen0226/mnguyen0226.github.io/blob/main/docs/resume/latest_resume_minh_nguyen.pdf"><h3>My Latest Resumé</h3></a>
+      {/* <a href="https://github.com/mnguyen0226/mnguyen0226.github.io/blob/main/docs/resume/latest_resume_minh_nguyen.pdf"><h3>My Latest Resumé</h3></a> */}
+      <h3>My Latest Resumé</h3>
     </div>
   </div>
 );
